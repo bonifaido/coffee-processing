e7f78dd8ee492554626f679d1a90b0942fd5931a
Nandor Kracser <bonifaido@gmail.com>
2012-09-01 21:41:56 +0200

    trading in tabs for to two spaces

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 function usage() {
-	print("Usage:")
-	print("\tcsp5 path/to/sketch")
-	quit()
+  print("Usage:")
+  print("  csp5 path/to/sketch")
+  quit()
 }
 
 if (arguments.length < 1) {
-	usage()
+  usage()
 }
 
 function loadCoffeeFile(path) {
-	var code = CoffeeScript.compile(readFile(path)) //, { bare: true })
-	eval(code)
+  var code = CoffeeScript.compile(readFile(path)) //, { bare: true })
+  eval(code)
 }
 
 var pathToSketch = arguments[0]
 
 
 if (/\.js$/.test(pathToSketch)) {
-	load(pathToSketch)
+  load(pathToSketch)
 } else if (/\.coffee$/.test(pathToSketch)) {
-	var CoffeeScript = require('coffee-script').CoffeeScript
-	loadCoffeeFile(pathToSketch)
+  var CoffeeScript = require('coffee-script').CoffeeScript
+  loadCoffeeFile(pathToSketch)
 }
